0f67f01bb6bc1f2379ed80aa5ea16335522a7d8e
Kento Sato <kento.s.account@gmail.com>
2013-12-26 21:15:55 -0800

    added examples, includes

---
+++ include/fdmi_exec.h
@@ -0,0 +1,6 @@
+#ifndef FDMI_EXEC_H
+#define FDMI_EXEC_H
+
+void reexec(void);
+
+#endif
---
+++ include/transfer.h
@@ -0,0 +1,17 @@
+#define CHUNK_SIZE (128 * 1024 * 1024)
+#define PATH_SIZE (128)
+#define NUM_BUFF (2)
+
+#define TEST_BUFF_SIZE (128 * 1024 * 1024)
+//#define TEST_BUFF_SIZE (4)
+
+//int id;
+
+struct scr_transfer_ctl {
+  char path[PATH_SIZE]; //path length
+  unsigned int size;
+  int id;
+};
+
+
+
